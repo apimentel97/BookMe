19be9493a9ea7a76004b396259b299299ded59ec
Alejandro Pimentel <apimentel97@yahoo.com>
2019-05-02 19:47:33 -0700

    Storyboard

---
+++ BookMe/AddPaymentViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddPaymentViewController.swift
+//  BookMe
+//
+//  Created by Alejandro pimentel on 5/2/19.
+//  Copyright © 2019 Alejandro pimentel. All rights reserved.
+//
+
+import UIKit
+
+class AddPaymentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BookMe/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  BookMe
+//
+//  Created by Alejandro pimentel on 5/2/19.
+//  Copyright © 2019 Alejandro pimentel. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
